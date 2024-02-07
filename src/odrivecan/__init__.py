0fd1504a654405ec46205216eea4444f2e6a72b7
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2024-02-07 05:18:56 +0000

    setting up pip package

---
+++ src/odrivecan/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+Examples
+---------
+    TODO
+"""
+__version__ = "0.0.1"
+
+from .odrivecan import hello
---
+++ src/odrivecan/odrivecan.py
@@ -0,0 +1,22 @@
+#This is where my package code will live. 
+
+
+
+
+def hello(word):
+    """
+    Docstring Example description.
+
+    Para:
+        word - this will take in a string and prints "Hello" + string.
+        
+
+    Example:
+        >>> import odrivecan
+        >>> odrive.hello("Dylan")
+        ...
+        ... Hello Dylan
+    """
+
+    #First example print function. 
+    print("Hello" + word)
